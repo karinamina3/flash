7522e0421bf4df18d233310e0d9c5c893a73b482
karinamina3 <00012217@uca.edu.sv>
2019-08-20 20:07:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: 'environment',
+    }
+  })
+  .then((stream) => {
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+    
+    // get the active track of the stream
+    const track = stream.getVideoTracks()[0];
+  
+    video.addEventListener('loadedmetadata', (e) => {  
+      window.setTimeout(() => (
+        onCapabilitiesReady(track.getCapabilities())
+      ), 500);
+    });
+  
+    function onCapabilitiesReady(capabilities) {
+      if (capabilities.torch) {
+        track.applyConstraints({
+          advanced: [{torch: true}]
+        })
+        .catch(e => console.log(e));
+      }
+    }
+    
+  })
+  .catch(err => console.error('getUserMedia() failed: ', err));
